fe7a839ff9f253d4166321afebc92d224a122848
ekimcem <ekim@bronixengineering.com>
2024-01-13 15:32:51 +0300

    delete try catch block

--- lib/queries/getAllPuzzleWalletEvents.ts
+++ lib/queries/getAllPuzzleWalletEvents.ts
@@ -6,17 +6,10 @@ import {
 } from "@puzzlehq/sdk";
 
 export const getAllPuzzleWalletEvents = async () => {
-  console.log("GET ALL PUZZLE WALLET EVENTS TRIGGERED");
   const filter: EventsFilter = {
     type: EventType.Execute,
     programId: "cassino_game_test_fp.aleo",
   };
-
-  try {
-    const events: GetEventsResponse = await getEvents(filter);
-
-    return events.events;
-  } catch (error) {
-    console.log(error);
-  }
+  const events: GetEventsResponse = await getEvents(filter);
+  return events.events;
 };
