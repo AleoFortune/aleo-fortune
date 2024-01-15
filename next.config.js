353086ee30c191165ea02f176b007b4d1a24bd2e
metehancaliskan <metehan.caliskan@ug.bilkent.edu.tr>
2024-01-15 19:08:26 +0300

    conjig was changed

--- next.config.js
+++ next.config.js
@@ -1,26 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: '/:path*',
-                headers: [
-                    {
-                        key: 'Cross-Origin-Opener-Policy',
-                        value: 'same-origin',
-                    },
-                    {
-                        key: 'Cross-Origin-Embedder-Policy',
-                        value: 'require-corp',
-                    },
-                ],
-            },
-        ]
-    },
-    webpack: (config) => {
-        config.experiments = { ...config.experiments, topLevelAwait: true };
-        return config;
-    },
 }
 
 module.exports = nextConfig
