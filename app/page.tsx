795c4304f298982d02598394ebe4323533a4e165
Osman Sait Güngör <osmansaitgungor@gmail.com>
2024-01-26 16:33:40 +0300

    making design responsive

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ import RightBar from "@/components/RightBar";
 
 export default function Home() {
   return (
-    <main className="flex gap-6 h-full" suppressHydrationWarning>
+    <main className="flex flex-col xl:flex-row gap-2 xl:gap-6 h-full px-10 xl:px-0" suppressHydrationWarning>
       <LeftBar className="w-60 overflow-auto max-h-screen" />
-      <MainContent className="flex-grow" />
-      <RightBar className="w-80 " />
+      <MainContent className="flex-grow order-2 xl:order-1" />
+      <RightBar className="w-full xl:w-80 order-1 xl:order-2" />
     </main>
   );
 }
--- components/LeftBar.tsx
+++ components/LeftBar.tsx
@@ -22,115 +22,82 @@ const LeftBar = (props: Props) => {
     <div
       className={cn(
         props.className,
-        "border-r-[1px] border-gray-500 bg-muted px-2 py-2 flex flex-col items-center gap-2"
+        "border-r-[1px] border-gray-500 bg-muted px-2 py-2 hidden xl:flex flex-col items-center gap-2"
       )}
     >
       {!account && <p>No account detected</p>}
       {account && (
-        <>
-          <Tabs defaultValue="depositHistory">
-            <TabsList className="grid w-full grid-cols-2">
-              <TabsTrigger value="depositHistory">Deposit History</TabsTrigger>
-              <TabsTrigger value="gameHistory">Game History</TabsTrigger>
-            </TabsList>
-            <TabsContent value="depositHistory">
-              <h3 className="font-bold">Deposit History</h3>
-              <Separator
-                orientation="horizontal"
-                className="mb-2 text-white bg-red-500"
-              />
-              <p className="text-muted-foreground text-sm font-semibold my-4">
-                You may also check executions in your Puzzle Wallet extension
-              </p>
-              {isLoading && <Spinner />}
-              {data?.map((e, index) => {
-                if (e.functionId == "deposit_public") {
-                  return (
-                    <Card className="w-full rounded-sm px-2 py-2 " key={index}>
-                      <CardTitle className="text-sm flex justify-between">
-                        Status:{" "}
-                        <Badge
-                          variant={
-                            (e.status == "Pending" && "destructive") ||
-                            "default"
-                          }
-                        >
-                          {e.status}
-                        </Badge>
-                      </CardTitle>
-                      <CardContent className="mt-2 p-0">
-                        <div className="flex flex-col gap-2">
-                          <p className="text-sm">
-                            <span className="font-bold">Amount:</span>{" "}
-                            <span className="text-red-300">
-                              {parseInt(e.inputs[1]!)}
-                            </span>{" "}
-                            Fortune Credits
-                          </p>
-                        </div>
-                      </CardContent>
-                    </Card>
-                  );
-                }
-              })}
-            </TabsContent>{" "}
-            <TabsContent value="gameHistory">
-              <h3 className="font-bold">Gameplay History</h3>
-              <Separator
-                orientation="horizontal"
-                className="mb-2 text-white bg-red-500"
-              />
-              <p className="text-muted-foreground text-sm font-semibold my-4">
-                You may also check executions in your Puzzle Wallet extension
-              </p>
-              {isLoading && <Spinner />}
-              {data?.map((e, index) => {
-                if (e.functionId == "make_straightup_bet_public") {
-                  return (
-                    <Card className="w-full  rounded-sm px-2 py-2 " key={index}>
-                      <CardTitle className="text-sm flex justify-between">
-                        Status:{" "}
-                        <Badge
-                          variant={
-                            (e.status == "Pending" && "destructive") ||
-                            "default"
-                          }
-                        >
-                          {e.status}
-                        </Badge>
-                      </CardTitle>
-                      <CardContent className="mt-2 p-0">
-                        <div className="flex flex-col gap-2">
-                          <p className="text-sm">
-                            <span className="font-bold">Bet Amount: </span>{" "}
-                            <span className="text-red-300">
-                              {parseInt(e.inputs[0]!)}
-                            </span>{" "}
-                            Fortune Credits
-                          </p>
-                        </div>
-                        <div className="flex flex-col gap-2">
-                          <p className="text-sm">
-                            <span className="font-bold">Bet: </span>{" "}
-                            <span className="text-red-300">
-                              {parseInt(e.inputs[1]!)}
-                            </span>{" "}
-                          </p>
-                        </div>
-                        <div className="flex flex-col gap-2">
-                          <p className="text-sm">
-                            <span className="font-bold">Game ID: </span>{" "}
-                            <span className="text-red-300">{e._id!}</span>{" "}
-                          </p>
-                        </div>
-                      </CardContent>
-                    </Card>
-                  );
-                }
-              })}
-            </TabsContent>
-          </Tabs>
-        </>
+        <Tabs defaultValue="depositHistory">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="depositHistory">Deposit History</TabsTrigger>
+            <TabsTrigger value="gameHistory">Game History</TabsTrigger>
+          </TabsList>
+          <TabsContent value="depositHistory">
+            <h3 className="font-bold">Deposit History</h3>
+            <Separator orientation="horizontal" className="mb-2 text-white bg-red-500" />
+            <p className="text-muted-foreground text-sm font-semibold my-4">
+              You may also check executions in your Puzzle Wallet extension
+            </p>
+            {isLoading && <Spinner />}
+            {data?.map((e, index) => {
+              if (e.functionId == "deposit_public") {
+                return (
+                  <Card className="w-full rounded-sm px-2 py-2 " key={index}>
+                    <CardTitle className="text-sm flex justify-between">
+                      Status: <Badge variant={(e.status == "Pending" && "destructive") || "default"}>{e.status}</Badge>
+                    </CardTitle>
+                    <CardContent className="mt-2 p-0">
+                      <div className="flex flex-col gap-2">
+                        <p className="text-sm">
+                          <span className="font-bold">Amount:</span>{" "}
+                          <span className="text-red-300">{parseInt(e.inputs[1]!)}</span> Fortune Credits
+                        </p>
+                      </div>
+                    </CardContent>
+                  </Card>
+                );
+              }
+            })}
+          </TabsContent>{" "}
+          <TabsContent value="gameHistory">
+            <h3 className="font-bold">Gameplay History</h3>
+            <Separator orientation="horizontal" className="mb-2 text-white bg-red-500" />
+            <p className="text-muted-foreground text-sm font-semibold my-4">
+              You may also check executions in your Puzzle Wallet extension
+            </p>
+            {isLoading && <Spinner />}
+            {data?.map((e, index) => {
+              if (e.functionId == "make_straightup_bet_public") {
+                return (
+                  <Card className="w-full  rounded-sm px-2 py-2 " key={index}>
+                    <CardTitle className="text-sm flex justify-between">
+                      Status: <Badge variant={(e.status == "Pending" && "destructive") || "default"}>{e.status}</Badge>
+                    </CardTitle>
+                    <CardContent className="mt-2 p-0">
+                      <div className="flex flex-col gap-2">
+                        <p className="text-sm">
+                          <span className="font-bold">Bet Amount: </span>{" "}
+                          <span className="text-red-300">{parseInt(e.inputs[0]!)}</span> Fortune Credits
+                        </p>
+                      </div>
+                      <div className="flex flex-col gap-2">
+                        <p className="text-sm">
+                          <span className="font-bold">Bet: </span>{" "}
+                          <span className="text-red-300">{parseInt(e.inputs[1]!)}</span>{" "}
+                        </p>
+                      </div>
+                      <div className="flex flex-col gap-2">
+                        <p className="text-sm">
+                          <span className="font-bold">Game ID: </span> <span className="text-red-300">{e._id!}</span>{" "}
+                        </p>
+                      </div>
+                    </CardContent>
+                  </Card>
+                );
+              }
+            })}
+          </TabsContent>
+        </Tabs>
       )}
     </div>
   );
